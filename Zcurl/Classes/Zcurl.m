7258c697b14bac8efbf8be2d335a54f7dbb4856f
lZackx <lzackx@lzackx.com>
2022-08-01 18:13:33 +0800

    update: class

---
+++ Zcurl/Classes/Zcurl.m
@@ -0,0 +1,12 @@
+//
+//  Zcurl.m
+//  Zcurl
+//
+//  Created by lZackx on 2022/8/1.
+//
+
+#import "Zcurl.h"
+
+@implementation Zcurl
+
+@end
---
+++ Zcurl/Classes/include/Zcurl.h
@@ -0,0 +1,16 @@
+//
+//  Zcurl.h
+//  Zcurl
+//
+//  Created by lZackx on 2022/8/1.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Zcurl : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
